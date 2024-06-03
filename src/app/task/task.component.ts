e15d015cc6b50d5dc1fb63cc9daf8990d0f7b2f7
Cleazyy <cleazy2002@gmail.com>
2024-06-03 07:30:50 +0200

    update task component

--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -1,12 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './task.component.html',
 })
 export class TaskComponent {
-  @Input({ required: true }) task!: string;
-  @Input({ required: true }) hour!: string;
+  @Input({ required: true }) task: string = '';
+  @Input({ required: true }) hour: string = '';
+  @Input({ required: true }) completed: boolean = false;
 }
