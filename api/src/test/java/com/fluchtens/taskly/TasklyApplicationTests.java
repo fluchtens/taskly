61895a8f786fc840975f9a117617e9e445bf262b
Cleazyy <cleazy2002@gmail.com>
2024-06-11 17:14:35 +0200

    add api folder

---
+++ api/src/test/java/com/fluchtens/taskly/TasklyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fluchtens.taskly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasklyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
