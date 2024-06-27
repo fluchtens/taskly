ccc7c83b259132da2a33fd54558284cc8a86efb1
Cleazyy <cleazy2002@gmail.com>
2024-06-27 11:01:55 +0200

    create prod dockerfile and nginx conf for client

--- client/prod.Dockerfile
+++ client/prod.Dockerfile
@@ -1,6 +1,7 @@
 FROM node:lts-alpine
 
-RUN apk update
+RUN apk update && \
+    apk add nginx
 
 WORKDIR /app
 
@@ -8,8 +9,10 @@ COPY package.json package-lock.json .
 RUN npm install
 COPY . .
 
+RUN npm run build
+
 COPY nginx.conf /etc/nginx/http.d/default.conf
 
-EXPOSE 4200
+EXPOSE 80
 
-CMD ["npm", "run", "dev"]
+CMD ["nginx", "-g", "daemon off;"]
--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -2,7 +2,7 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { DatePipe } from '@angular/common';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideClientHydration } from '@angular/platform-browser';
 import { routes } from './app.routes';
 
@@ -11,7 +11,7 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideClientHydration(),
-    provideHttpClient(),
+    provideHttpClient(withFetch()),
     DatePipe,
   ],
 };
